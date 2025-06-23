08ea0bc779425ad9d8550e38403668e266be4f98
mari3-lima <104018340+mari3-lima@users.noreply.github.com>
2025-06-23 20:56:29 -0300

    Update main.c
    Inclusão da lista

--- main.c
+++ main.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "interpret.h"
 #include "stack.h"
+#include "list.h"
+
+// Declaração das variáveis globais, que já estão definidas em interpret.c
+extern Stack *s;
+extern List *l;
 
 static void repl()
 {
-  char line[1024];
-  for (;;)
-  {
-    printf("> ");
-
-    if (!fgets(line, sizeof(line), stdin))
+    char line[1024];
+    for (;;)
     {
-      printf("\n");
-      break;
-    }
+        printf("> ");
 
-    interpret(line);
-  }
+        if (!fgets(line, sizeof(line), stdin))
+        {
+            printf("\n");
+            break;
+        }
+
+        if (strncmp(line, "exit", 4) == 0)
+            break;
+
+        interpret(line);
+    }
 }
 
 int main () {
-  s_init();
-  repl();
-  return 0;
+    s_init();
+    repl();
+    return 0;
 }
